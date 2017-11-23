dc854431d85f3eaf706752c65c51e21670f5f41f
solufemi91 <stevenoo@spartaglobal.com>
2017-11-23 22:53:34 +0000

    added a user_id column

---
+++ db/migrate/20171123225212_add_column_to_songs3.rb
@@ -0,0 +1,5 @@
+class AddColumnToSongs3 < ActiveRecord::Migration[5.1]
+  def change
+    add_column :songs, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123223138) do
+ActiveRecord::Schema.define(version: 20171123225212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171123223138) do
     t.datetime "updated_at", null: false
     t.string "song_name"
     t.integer "year"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
